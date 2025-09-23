aa57b1541b034f81e4c50e46d31e642ccab6b554
Thanankorn Choonin <thanankorn.c@ku.th>
2025-09-23 14:55:16 +0700

    45

--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,19 @@
 import random
 
 def test_random():
-    random_number = random.randint(1, 100)
-    print(random_number)
+    random_number = random.randint(1, 10) #สุ่มตัวเขระหว่าง 1-10
+    print("== Guess  Number between (1-10) ==")
+    guess_number = input("What is your guess number:")
+    guess_number = int(guess_number)
+
+    if random_number == guess_number:
+        print("Congratulations")
+
+    if random_number < guess_number:
+        print("Too High")    
+
+    if random_number > guess_number:    
+        print("Too Low")
+
     
 test_random()
